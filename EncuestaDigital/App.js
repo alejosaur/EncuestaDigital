d591218dc20fa95582a6824a865f92fca45b27db
alejosaur <asantamariap@unal.edu.co>
2018-08-19 20:06:15 -0500

    yupi

--- EncuestaDigital/App.js
+++ EncuestaDigital/App.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import DatePicker from 'react-native-datepicker'
 
 class HomeScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <DatePicker
+          style={{width: 200}}
+          date={this.state.date}
+          mode="date"
+          placeholder="select date"
+          format="YYYY-MM-DD"
+          minDate="2015-05-01"
+          maxDate="2125-06-01"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0
+            },
+            dateInput: {
+              marginLeft: 36
+            }
+            // ... You can check the source to find the other keys.
+          }}
+          onDateChange={(date) => {this.setState({date: new date()})}}
+      />
+
+        <TextInput
+          style={{height: 40, width: 120                                                                                                                                                                                                                                                                                                                                          , borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          underlineColorAndroid={'transparent'}
+          value={this.state.text}
+        />
+        <TextInput
+          style={{height: 40, width: 120, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          underlineColorAndroid={'transparent'}
+          value={this.state.text}
+        />
         <Button
-          title="Go to Details"
+          title="Continuar"
           onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
+            /* 1. Navigate to the Encuesta route with params */
+            this.props.navigation.navigate('Encuesta', {
               itemId: 86,
               otherParam: 'anything you want here',
             });
@@ -22,7 +65,7 @@ class HomeScreen extends React.Component {
   }
 }
 
-class DetailsScreen extends React.Component {
+class EncuestaScreen extends React.Component {
   render() {
     /* 2. Get the param, provide a fallback value if not available */
     const { navigation } = this.props;
@@ -30,21 +73,9 @@ class DetailsScreen extends React.Component {
     const otherParam = navigation.getParam('otherParam', 'some default value');
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+      <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start' }}>
         <Text>itemId: {JSON.stringify(itemId)}</Text>
         <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() =>
-            this.props.navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100),
-            })}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
         <Button
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
@@ -57,7 +88,7 @@ class DetailsScreen extends React.Component {
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Encuesta: EncuestaScreen,
   },
   {
     initialRouteName: 'Home',
@@ -69,4 +100,3 @@ export default class App extends React.Component {
     return <RootStack />;
   }
 }
-
