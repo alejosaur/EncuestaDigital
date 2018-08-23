48f62b93cf98f8dea37c84993b603b6f41e4f301
federgolo <fgomezl@unal.edu.co>
2018-08-22 21:04:13 -0500

    installed drop down menu, added example to screen

--- EncuestaDigital/App.js
+++ EncuestaDigital/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Button, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import DatePicker from 'react-native-datepicker'
+import DropdownMenu from 'react-native-dropdown-menu';
+
 
 class HomeScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { textEncuestador: '' ,  textConsecutivo: '' ,date: new Date()};
+    this.state = { textEncuestador: '' ,  textConsecutivo: '' ,date: new Date(), text:''};
   }
 
   render() {
+    var data = [["C", "Java", "JavaScript", "PHP"], ["Python", "Ruby"], ["Swift", "Objective-C"]];
     return (
       <View style={styles.cuerpo} >
         <View style={styles.lateral}>
@@ -86,6 +89,31 @@ class HomeScreen extends React.Component {
 
           </View>
         </View>
+
+        <View style={{flex: 1}}>
+        <View style={{height: 64}} />
+        <DropdownMenu
+          style={{flex: 1}}
+          bgColor={'white'}
+          tintColor={'#666666'}
+          activityTintColor={'green'}
+          // arrowImg={}      
+          // checkImage={}   
+          // optionTextStyle={{color: '#333333'}}
+          // titleStyle={{color: '#333333'}} 
+          // maxHeight={300} 
+          handler={(selection, row) => this.setState({text: data[selection][row]})}
+          data={data}
+        >
+ 
+          <View style={{flex: 1}}>
+            <Text>
+              {this.state.text} is the best language in the world
+            </Text>
+          </View>
+ 
+        </DropdownMenu>
+      </View>    	
 
         <Button
           title="Continuar"
